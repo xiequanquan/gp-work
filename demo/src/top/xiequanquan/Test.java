8e83c34a2688b060f63c3bcdee02a1440b9e7b5b
xiequanquan <q.uan163@163.com>
2019-03-15 16:43:26 +0800

    update

---
+++ demo/src/top/xiequanquan/Test.java
@@ -0,0 +1,12 @@
+package top.xiequanquan;
+
+/**
+ * @author XQQ
+ * @create 2019-03-15 16:41
+ * @desc 测试
+ **/
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello word");
+    }
+}
